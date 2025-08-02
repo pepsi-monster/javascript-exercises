258060cce1db7b2283086a68d43ba6a918897f09
Johnny Noh <ima.john.noh@gmail.com>
2025-08-03 01:25:46 +0900

    solve 06, 07

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (str, times) {
+  let result = "";
+  if (times < 0) {
+    console.error("Times must be positive integer");
+    return (result = "ERROR");
+  } else {
+    for (let i = 0; i < times; i++) {
+      result += str;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let j = str.length - 1;
+  const arr = str.split("");
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = str[j];
+    j--;
+  }
+  return arr.join("");
 };
+
+let string = "doctor";
+console.log(reverseString(string));
 
 // Do not edit below this line
 module.exports = reverseString;
