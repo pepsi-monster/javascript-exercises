cc154f6fe6edd63e14d9a0d9c8dc129f205d6f4e
Johnny Noh <ima.john.noh@gmail.com>
2025-08-04 11:04:27 +0900

    Solve 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (a * -1 > 0 || b * -1 > 0) {
+    return "ERROR";
+  } else if (a % 1 != 0 || b % 1 != 0) {
+    return "ERROR";
+  } else if (typeof a != "number" || typeof b != "number") {
+    return "ERROR";
+  }
 
+  const arr = [];
+  if (a >= b) {
+    while (a >= b) {
+      arr.push(b++);
+    }
+  } else if (a <= b)
+    while (a <= b) {
+      arr.push(a++);
+    }
+  return arr.reduce((total, each) => (total += each));
 };
 
 // Do not edit below this line
