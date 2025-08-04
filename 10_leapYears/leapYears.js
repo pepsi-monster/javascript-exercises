7ba075fc18d27c69a69a1cc4ec07dce16480f31e
Johnny Noh <ima.john.noh@gmail.com>
2025-08-04 13:40:19 +0900

    Solve 10 leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+/*
+const leapYears = function (year) {
+  if (year % 4 === 0 && year % 100 != 0) {
+    return true;
+  } else if (year % 400 === 0) return true;
+  else return false;
 };
+*/
+
+const leapYears = function (year) {
+  return (year % 4 === 0 && year % 100 != 0) || year % 400 === 0 ? true : false;
+};
+console.log(leapYears(700));
 
 // Do not edit below this line
 module.exports = leapYears;
