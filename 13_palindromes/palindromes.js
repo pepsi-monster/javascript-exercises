b7e9150138993034158b222ab5c038f8801a1298
Johnny Noh <ima.john.noh@gmail.com>
2025-08-26 03:28:18 +0900

    Solve 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const forwardSpelled = str
+    .toLowerCase()
+    .match(/[a-zA-Z0-9]+/g)
+    .join("");
+  const backwardSpelled = forwardSpelled.split("").reverse().join("");
 
+  return forwardSpelled === backwardSpelled ? true : false;
 };
 
 // Do not edit below this line
